18404ea976bdda55f13328a90e849cfee869c56e
T.Y.M.SAI <tymsai6076@gmail.com>
2021-07-18 11:02:12 +0530

    Add files via upload

---
+++ resource/app.js
@@ -0,0 +1,6 @@
+function slider() {
+    var colorchangeone=document.getElementById('nameform')
+    var colorchangetwo=document.getElementById('msgform')
+    colorchangeone.classList.toggle('changu')
+    colorchangetwo.classList.toggle('changu')
+}
